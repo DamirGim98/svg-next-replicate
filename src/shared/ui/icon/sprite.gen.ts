359e000e9510880e42dce3c71df5cf42b2b32a67
Gimatdinov Damir <damir.gimatdinov@rt.ru>
2023-10-09 09:38:11 +0500

    Setup commit

---
+++ src/shared/ui/icon/sprite.gen.ts
@@ -0,0 +1,16 @@
+export interface SpritesMap {
+    'sprite': 'add' | 'autoscroll-from-source' | 'autoscroll-to-source' | 'build-server-protocol' | 'build' | 'c-make-tool-window' | 'changes' | 'checkmark' | 'commit'
+  }
+export const SPRITES_META = {
+    'sprite': ['add',
+'autoscroll-from-source',
+'autoscroll-to-source',
+'build-server-protocol',
+'build',
+'c-make-tool-window',
+'changes',
+'checkmark',
+'commit']
+  } satisfies {
+      'sprite': Array<'add' | 'autoscroll-from-source' | 'autoscroll-to-source' | 'build-server-protocol' | 'build' | 'c-make-tool-window' | 'changes' | 'checkmark' | 'commit'>
+    };
